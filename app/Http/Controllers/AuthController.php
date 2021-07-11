11f660fc85af4166ac1ff7abd5daef3a2d4deebd
Rifat Al- Ashwad <rifatbilalphilips@gmail.com>
2021-07-11 13:11:11 +0600

    unserialize

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -42,6 +42,8 @@ class AuthController extends Controller
 
         $accessToken = auth()->user()->createToken('authToken')->accessToken;
         $role = auth()->user()->role;
+        $role = unserialize($role);
+        return $role;
         auth()->user()->role = unserialize($role);
 
         return response()->json(['user' => auth()->user(), 'token' => $accessToken,   "ok" => true], 200);
