a63b9b70d2beb9a3b274e3f3e69ef051cdd547ab
Rifat Al- Ashwad <rifatbilalphilips@gmail.com>
2021-07-21 18:51:30 +0600

    single course secure

--- app/Http/Controllers/ClientCourseController.php
+++ app/Http/Controllers/ClientCourseController.php
@@ -27,41 +27,34 @@ class ClientCourseController extends Controller
             ])
             ->first();
         if (count((array)$courses)) {
-            if ($courses->paid) {
-                $lessonsFree = DB::table('lessons')
-                    ->where([
-                        "course_id" => $courses->id,
-                        "free_preview" => true
-                    ])
-                    ->get()->toArray();
-                $lessonsPaid =  DB::table('lessons')
-                    ->where([
-                        "course_id" => $courses->id,
-                        "free_preview" => false
-                    ])
-                    ->select(
-                        "id",
-                        'title',
-                        "slug",
-                        "content",
-                        "free_preview",
-                        "course_id",
-                        "instructor_id",
-                        "instructor_name",
-                        "custom_id",
-                        "created_at",
-                        "updated_at"
-                    )
-                    ->get()->toArray();
-                $lessons = array_merge($lessonsFree, $lessonsPaid);
-            } else {
-                $lessons = DB::table('lessons')
-                    ->where([
-                        "course_id" => $courses->id,
-                    ])
-                    ->orderBy("custom_id")
-                    ->get();
-            }
+
+            $lessonsFree = DB::table('lessons')
+                ->where([
+                    "course_id" => $courses->id,
+                    "free_preview" => true
+                ])
+                ->get()->toArray();
+            $lessonsPaid =  DB::table('lessons')
+                ->where([
+                    "course_id" => $courses->id,
+                    "free_preview" => false
+                ])
+                ->select(
+                    "id",
+                    'title',
+                    "slug",
+                    "content",
+                    "free_preview",
+                    "course_id",
+                    "instructor_id",
+                    "instructor_name",
+                    "custom_id",
+                    "created_at",
+                    "updated_at"
+                )
+                ->get()->toArray();
+            $lessons = array_merge($lessonsFree, $lessonsPaid);
+
 
             return response()->json([
                 "courses" => $courses,
