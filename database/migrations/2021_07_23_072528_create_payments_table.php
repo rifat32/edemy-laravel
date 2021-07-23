af76aa55254f23899f74126317c3f49bfa58593b
Rifat Al- Ashwad <rifatbilalphilips@gmail.com>
2021-07-23 13:28:06 +0600

    payment table

---
+++ database/migrations/2021_07_23_072528_create_payments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->string("status")->default("pending");
+            $table->string("payment_details");
+            $table->string("course_slug");
+            $table->string("price");
+            $table->string("contact_info");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
